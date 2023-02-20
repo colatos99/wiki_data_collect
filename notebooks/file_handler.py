87a3d500e2a454c112096540d4336d8148041da9
Colatos99 <micahrumsey18.gmail.com>
2023-02-20 09:29:45 -0500

    saving text fiiles working

--- notebooks/file_handler.py
+++ notebooks/file_handler.py
@@ -1,9 +1,10 @@
 import os
-home_dir = "E:\python_projects\wiki_data_collect"
-data_dir = "\data"
-data_path_full = os.path.join(home_dir, data_dir)
-page_text_dir = "\wiki_pages"
-page_path_full = os.path.join(home_dir, page_text_dir)
+import io
+home_dir =".wiki_data_collect"
+data_dir = "data"
+data_path_full = "E:\python_projects\wiki_data_collect\wiki_pages\\"
+page_text_dir = "wiki_pages/"
+page_path_full = "E:\python_projects\wiki_data_collect\wiki_pages\\"
 print(data_path_full)
 
 def get_page_path_full():
@@ -11,5 +12,6 @@ def get_page_path_full():
 def get_data_path_full():
     return data_path_full
 
-def  write_page_text(page_name):
-    
+def  write_page_text(page_name=str, page=str):
+    with open( page_path_full + str(page_name) + ".txt", 'w', encoding= 'utf-8' ) as f:
+        f.write(page)
