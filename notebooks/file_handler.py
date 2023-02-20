41fecbe9b3c9c128e03f5a057b0f80d011a70ec7
Colatos99 <micahrumsey18.gmail.com>
2023-02-20 13:48:09 -0500

    added pickle file for dcsv copy info

--- notebooks/file_handler.py
+++ notebooks/file_handler.py
@@ -1,9 +1,10 @@
 import os
 import io
-home_dir =".wiki_data_collect"
-data_dir = "data"
-data_path_full = "E:\python_projects\wiki_data_collect\wiki_pages\\"
-page_text_dir = "wiki_pages/"
+import pandas as pd
+
+
+data_path_full = "E:\python_projects\wiki_data_collect\data\\"
+
 page_path_full = "E:\python_projects\wiki_data_collect\wiki_pages\\"
 print(data_path_full)
 
@@ -13,5 +14,18 @@ def get_data_path_full():
     return data_path_full
 
 def  write_page_text(page_name=str, page=str):
-    with open( page_path_full + str(page_name) + ".txt", 'w', encoding= 'utf-8' ) as f:
-        f.write(page)
+    with open( page_path_full + str(page_name) + ".txt", 'x', encoding= 'utf-8' ) as page_file:
+        if page_file.readlines() != None:
+            print("File Already exists!")
+        else:
+            page_file.write(page)
+
+def create_init_main_csv():
+    csv_num = 1
+    csv_info = {"csv_copy_num : [1]"}
+    csv_info_df = pd.DataFrame(csv_info)
+    csv_info_df.to_pickle(data_path_full + "csv_copy_num_store.pickle")
+    
+def show_csv_copy_info():
+    info = pd.read_pickle(data_path_full + "csv_copy_num_store.pickle")
+    return info
