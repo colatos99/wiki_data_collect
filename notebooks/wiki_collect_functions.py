ebcf9ecb13b01f3169234417c58e3b6949b12bcb
Colatos99 <micahrumsey18.gmail.com>
2023-02-23 13:37:17 -0500

    updated wiki_collectt_functions

--- notebooks/wiki_collect_functions.py
+++ notebooks/wiki_collect_functions.py
@@ -2,14 +2,22 @@
 import wikipediaapi
 
 
+def setup_wiki_api():
+    wiki_wiki =  wikipediaapi.Wikipedia(
+    language = 'en',
+    extract_format=wikipediaapi.ExtractFormat.WIKI)
+    return wiki_wiki
 
+    
+'''
 wiki_wiki = wikipediaapi.Wikipedia(
     language = 'en',
     extract_format=wikipediaapi.ExtractFormat.WIKI
 )
-
+'''
 
 
 def get_page_full(wiki_page_name):
-    p_wiki = wiki_wiki.page(wiki_page_name) 
-    return p_wiki
+    p_wiki = setup_wiki_api()
+    full_page = p_wiki.page(wiki_page_name) 
+    return full_page
